3cddf29327d2bafcf5b6d50379397bbbc4ffa7d9
Bohdan Skibitskyi <bogdanskibitskyi@gmail.com>
2026-07-14 20:34:02 +0200

    feat: add base Entity class

--- mario_stufe1/include/Entity.h
+++ mario_stufe1/include/Entity.h
@@ -2,7 +2,11 @@
 #include <SFML/Graphics.hpp>
 
 // Basisklasse fuer alle Spielobjekte (Entities).
-// Jede Entity hat eine Position, eine Geschwindigkeit und einen Bounding-Box.
+// Jede Entity hat eine Position, eine Geschwindigkeit, eine feste Groesse und
+// eine daraus abgeleitete Bounding-Box. Die Bounding-Box wird IMMER automatisch
+// aktualisiert (setPosition/move/setSize) - das ist wichtig fuer Etappe 2:
+// das CollisionSystem verlaesst sich darauf, dass getBounds() nach jedem
+// setPosition() sofort korrekt ist, ohne dass jemand manuell "updateBounds" ruft.
 class Entity {
 public:
     explicit Entity(sf::Vector2f position = {0.f, 0.f});
@@ -18,21 +22,26 @@ public:
 
     const sf::Vector2f& getPosition() const noexcept { return m_position; }
     const sf::Vector2f& getVelocity() const noexcept { return m_velocity; }
+    sf::Vector2f getSize() const noexcept { return m_size; }
     const sf::FloatRect& getBounds() const noexcept { return m_bounds; }
 
-    void setPosition(const sf::Vector2f& position) noexcept { m_position = position; }
+    void setPosition(const sf::Vector2f& position) noexcept;
     void setVelocity(const sf::Vector2f& velocity) noexcept { m_velocity = velocity; }
-    void move(const sf::Vector2f& offset) noexcept { m_position += offset; }
+    void move(const sf::Vector2f& offset) noexcept;
 
     bool isAlive() const noexcept { return m_alive; }
     void kill() noexcept { m_alive = false; }
 
 protected:
-    // Aktualisiert die Bounding-Box relativ zur aktuellen Position.
-    void updateBounds(sf::Vector2f size) noexcept;
+    // Wird von abgeleiteten Klassen einmalig gesetzt (i.d.R. im Konstruktor).
+    void setSize(sf::Vector2f size) noexcept;
 
     sf::Vector2f m_position;
     sf::Vector2f m_velocity{0.f, 0.f};
+    sf::Vector2f m_size{0.f, 0.f};
     sf::FloatRect m_bounds;
     bool m_alive{true};
+
+private:
+    void refreshBounds() noexcept;
 };
--- mario_stufe1/src/Entity.cpp
+++ mario_stufe1/src/Entity.cpp
@@ -3,6 +3,21 @@
 Entity::Entity(sf::Vector2f position)
     : m_position(position) {}
 
-void Entity::updateBounds(sf::Vector2f size) noexcept {
-    m_bounds = sf::FloatRect(m_position, size);
+void Entity::setPosition(const sf::Vector2f& position) noexcept {
+    m_position = position;
+    refreshBounds();
+}
+
+void Entity::move(const sf::Vector2f& offset) noexcept {
+    m_position += offset;
+    refreshBounds();
+}
+
+void Entity::setSize(sf::Vector2f size) noexcept {
+    m_size = size;
+    refreshBounds();
+}
+
+void Entity::refreshBounds() noexcept {
+    m_bounds = sf::FloatRect(m_position, m_size);
 }
